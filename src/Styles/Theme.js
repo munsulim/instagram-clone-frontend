9ac2688ba7f66bf4f7fe5fcf226683651286e2fe
munsu <38244766+munsulim@users.noreply.github.com>
2020-05-07 00:13:33 +0900

    기타: css 기본 설정.
    스타일 컴포넌트의 글로벌 스타일과 테마에 기본 css 설정을
    추가하였습니다.

---
+++ src/Styles/Theme.js
@@ -0,0 +1,17 @@
+const BOX_BORDER = "border: 1px solid rgba(var(--b6a,219,219,219),1)";
+const BORDER_RADIUS = "4px";
+
+export default {
+  bgColor: "#FAFAFA",
+  blackColor: "#262626",
+  blueColor: "#0095F6",
+  darkGreyColor: "#8E8E8E",
+  lightGreyColor: "#C7C7C7",
+  redColor: "#ED4956",
+  darkBlueColor: "#00376b",
+  boxBorder: `${BOX_BORDER};`,
+  borderRadius: `${BORDER_RADIUS}`,
+  whiteBox: `${BORDER_RADIUS};
+  ${BOX_BORDER};
+  background-color:white;`,
+};
